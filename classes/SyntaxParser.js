e8ba5c6771103a29086e4982080cdc6b750f892c
Jakub Vrba <kubo.vrba@gmail.com>
2016-12-05 21:03:45 +0100

    Adds brutal SWAG to output

--- classes/SyntaxParser.js
+++ classes/SyntaxParser.js
@@ -28,8 +28,24 @@ class SyntaxParser {
     }
 
     checkSyntax() {
+        var outputArea = document.getElementById("output-area");
 
         while ((this.inputStack.length !== 0) && (this.parsingStack.length !== 0)) {
+
+            outputArea.value += 'Input: '
+            for(var y = this.inputStack.length - 1; y>= 0; y--){
+                outputArea.value += this.inputStack[y].key + ' ';
+            }
+            outputArea.value += '\n'
+
+            outputArea.value += 'Stack: '
+            for(var z = this.parsingStack.length - 1; z>= 0; z--){
+                console.log(this.parsingStack[z]);
+                outputArea.value += this.parsingStack[z] + ' ';
+            }
+            outputArea.value += '\n'
+
+
 
             let actualNonTerminal = this.parsingStack.pop();
             let actualToken = this.inputStack[this.inputStack.length - 1].key;
@@ -44,19 +60,28 @@ class SyntaxParser {
             }
 
             if (ruleRank === 'A') {
+                outputArea.value += 'Accepting input \n';
                 this.inputStack.pop();
                 break;
             }
             else if (ruleRank === 'z') {
+                outputArea.value += 'Consume token from input \n';
                 this.inputStack.pop();
             }
             else {
                 const selectedRule = this.rules.rulesObj[ruleRank];
-
+                outputArea.value += 'Using rule number: ' + ruleRank + ', ' + selectedRule.leftSide + ' -> ';
                 for (var i = selectedRule.rightSide.length - 1; i >= 0; i--) {
+                    outputArea.value += selectedRule.rightSide[i] + ' ';
                     this.parsingStack.push(selectedRule.rightSide[i]);
                 }
+                outputArea.value += '\n';
             }
+
+            for(var t = 0; t < 175; t++){
+                outputArea.value += '-';
+            }
+            outputArea.value += '\n'
 
         }
 
